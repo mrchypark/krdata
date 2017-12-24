a9964d6abc987c3fc80763ea0ff984348c5b0474
ChanYub.Park <mrchypark@gmail.com>
2017-12-24 18:53:37 +0900

    get apply url

---
+++ R/get_apply_url.R
@@ -0,0 +1,11 @@
+#' Get apply url
+#'
+#' Get apply url for krdata list.
+#'
+#' @return print apply url
+#' @export
+
+get_apply_url <- function(){
+  tar <- "https://goo.gl/forms/5DL09FLNd04seBoD2"
+  print(tar)
+}
